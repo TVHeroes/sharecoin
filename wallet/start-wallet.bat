9600ac4ab0cdeaa8301920b278a18c02c34291f8
tvheroes <tvheroes@protonmail.com>
2026-07-19 22:08:11 +0100

    Fix start-wallet.bat: create datadir if missing

--- wallet/start-wallet.bat
+++ wallet/start-wallet.bat
@@ -2,4 +2,5 @@
 REM Launches the Sharecoin wallet GUI, connected to the live public node
 REM over Tailscale Funnel (see this repo's README for what that means and
 REM how to reach it without installing Tailscale yourself, if applicable).
+if not exist "%~dp0datadir" mkdir "%~dp0datadir"
 "%~dp0sharecoin-qt.exe" -regtest -datadir="%~dp0datadir" -connect=stormforge.tail0b8084.ts.net:8443
